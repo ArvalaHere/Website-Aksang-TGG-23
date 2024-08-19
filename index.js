69ed100930203a36fc947486b1b739b2ed0cbc64
Astravy <73333715+ArvalaHere@users.noreply.github.com>
2024-08-19 15:52:15 +0700

    nyobain aja

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 var tes = 'komm susser tod'
 console.log(tes)
+
+// Tes Looping
+console.log("for loop test")
+var mobil = [99,54,23,77]
+for (var i=0 ; i<mobil.length ; i++) {
+    console.log(mobil[i])
+}
+
+console.log("while loop test")
+var j=0
+while (j<mobil.length) {
+    if (mobil[j]==23) {
+        console.log("yang ini")
+    } else {
+        console.log("bukan yang ini")
+    }
+    j++
+}
