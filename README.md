8f7c78ace2190c8142f9f8e6e3b6af530afde906
firzatullah al ghiffari <firzatullahalghiffari@gmail.com>
2024-09-19 00:13:36 +0700

    Membuat tampilan sesuai Figma

--- README.md
+++ README.md
@@ -4,3 +4,10 @@ Website Aksi Angkatan Teknik Geodesi dan Geomatika 2023
 GRAND DESIGN
 main font pake font-body
 warna ada PrimaryColor dan SecondaryColor
+
+Daftar color hex untuk background 
+- Warna background utama: #fffbf0
+- Warna background paling akhir: #310600
+
+Silakan gunakan pragmatisasi bg-[kode hex] agar lebih cepat dan mudah (kalau Arya gak marah).
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,13 +6,35 @@ module.exports = {
     extend: {
       colors: {
         BackgroundColor: '#FAF0B9',
-        SecondaryColor: '#AF3700'
-    },
+        SecondaryColor: '#AF3700',
+        BgAwal: '#fffbf0',
+      },
+      boxShadow: {
+        custom: '2px 2px 8px rgba(0, 0, 0, 0.2)',
+      },
       fontFamily: {
-        body: ["Montserrat"]
-      }
+        body: ["Montserrat"],
+        main: ["Adventure"],
+      },
+      fontSize: {
+        h1: ['64px', '120%'],
+        h2: ['48px', '120%'],
+        h3: ['32px', '120%'],
+        h4: ['24px', '120%'],
+        h5: ['20px', '120%'],
+        h6: ['16px', '120%'],
+        sh1: ['36px', '120%'],
+        sh2: ['32px', '120%'],
+        sh3: ['24px', '120%'],
+        sh4: ['20px', '120%'],
+        sh5: ['18px', '120%'],
+        b1: ['24px', '120%'],
+        b2: ['20px', '120%'],
+        b3: ['16px', '120%'],
+        b4: ['14px', '120%'],
+        b5: ['12px', '120%'],
+      },
+    },
   },
   plugins: [],
 }
-}
-
